97e3274dc44b5c532f4cf77abca41ea27ffbb0fa
CodeSandbox <support@codesandbox.io>
2025-04-23 00:22:50 +0000

    commit to main

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,4 +21,25 @@ class ProductController extends Controller
         $products = $this->productService->getAllProducts();
         return view('products.index', compact('products'));
     }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'category_id' => 'required|exists:categories,id',
+            'bpom_code' => 'nullable|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+            'is_by_order' => 'required|boolean',
+        ]);
+
+        $this->productService->createProduct($data);
+
+        return redirect()->route('products.index')->with('success', 'Product created successfully.');
+    }
 }
